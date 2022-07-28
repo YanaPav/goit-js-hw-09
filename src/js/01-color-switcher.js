0ca902423e003116dffd1460d433ed49eed983c1
Yana Pavlyk <pavlikyg@gmail.com>
2022-07-28 19:58:13 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+let timerId = null
+stopBtn.setAttribute('disabled', true)
+
+startBtn.addEventListener('click', onStartBtnClick)
+
+function onStartBtnClick() {
+    startBtn.setAttribute('disabled', true)
+    stopBtn.removeAttribute('disabled')
+    timerId = setInterval(changeColor, 1000)
+    stopBtn.addEventListener('click', onStopBtnClick)
+}
+
+function onStopBtnClick() {
+    startBtn.removeAttribute('disabled')
+    stopBtn.setAttribute('disabled', true)
+    clearInterval(timerId)
+    startBtn.addEventListener('click', onStartBtnClick)
+}
+
+function changeColor() {
+    document.body.style.backgroundColor =getRandomHexColor()
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 
