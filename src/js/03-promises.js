60ab4e859e2ce26e8b4418524e35075a231feb5c
Yana Pavlyk <pavlikyg@gmail.com>
2022-07-30 15:45:43 +0300

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix'
+
+const form = document.querySelector('.form')
+form.addEventListener('submit', onFormSubmit)
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);    
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    },delay)
+    
+  })
+  
+}
+
+function onFormSubmit(e) {
+  e.preventDefault()
+  const firstDelay = Number(e.currentTarget.elements.delay.value)
+  const stepValue = Number(e.currentTarget.elements.step.value)
+  const amount = Number(e.currentTarget.elements.amount.value)
+
+  for (i = 0; i < amount; i += 1) {
+    const step = firstDelay + stepValue * i
+    const position = i + 1
+    
+      createPromise(position, step)
+      .then(value => { Notiflix.Notify.success(value) })
+      .catch(error => { Notiflix.Notify.failure(error) })  
   }
+
 }
